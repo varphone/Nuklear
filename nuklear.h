4ba2f40189c2eafc36a9ad560451e86a2b881734
Varphone Wong <varphone@qq.com>
2020-06-05 18:09:40 +0800

    Add new api: nk_layout_row_colored()
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- nuklear.h
+++ nuklear.h
@@ -2411,6 +2411,22 @@ NK_API void nk_layout_row_end(struct nk_context*);
 /// __columns__ | Number of widget inside row
 */
 NK_API void nk_layout_row(struct nk_context*, enum nk_layout_format, float height, int cols, const float *ratio);
+/*/// #### nk_layout_row_colored
+/// Specifies row columns in array as either window ratio or size with background color
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~c
+/// void nk_layout_row_colored(struct nk_context*, enum nk_layout_format, float height, int cols, const float *ratio, struct nk_color color);
+/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+///
+/// Parameter   | Description
+/// ------------|-----------------------------------------------------------
+/// __ctx__     | Must point to an previously initialized `nk_context` struct after call `nk_begin_xxx`
+/// __fmt__     | Either `NK_DYNAMIC` for window ratio or `NK_STATIC` for fixed size columns
+/// __height__  | Holds height of each widget in row or zero for auto layouting
+/// __columns__ | Number of widget inside row
+*/
+NK_API void
+nk_layout_row_colored(struct nk_context *ctx, enum nk_layout_format fmt,
+    float height, int cols, const float *ratio, struct nk_color color);
 /*/// #### nk_layout_row_template_begin
 /// Begins the row template declaration
 /// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~c
@@ -21524,6 +21540,36 @@ nk_layout_row(struct nk_context *ctx, enum nk_layout_format fmt,
     }
     layout->row.item_offset = 0;
     layout->row.filled = 0;
+}
+NK_API void
+nk_layout_row_colored(struct nk_context *ctx, enum nk_layout_format fmt,
+    float height, int cols, const float *ratio, struct nk_color background)
+{
+    struct nk_style *style;
+    struct nk_window *win;
+    struct nk_color old_background;
+    int old_flags;
+
+    NK_ASSERT(ctx);
+    NK_ASSERT(ctx->current);
+    NK_ASSERT(ctx->current->layout);
+    if (!ctx || !ctx->current || !ctx->current->layout)
+        return;
+
+    style = &ctx->style;
+    win = ctx->current;
+
+    /* Save current states */
+    old_background = style->window.background;
+    old_flags = win->layout->flags;
+
+    style->window.background = background;
+    win->layout->flags |= NK_WINDOW_DYNAMIC;
+    nk_layout_row(ctx, fmt, height, cols, ratio);
+
+    /* Restore saved states */
+    style->window.background = old_background;
+    win->layout->flags = old_flags;
 }
 NK_API void
 nk_layout_row_template_begin(struct nk_context *ctx, float height)
